0e9c5c658f68e474baf06499a1c114be041ae6b0
Patrick Wang <wzhpanging@163.com>
2018-03-13 00:38:42 +0800

    modified
    main

--- Pandar_test/main.cpp
+++ Pandar_test/main.cpp
@@ -1,16 +1,60 @@
-#include "LidarTools.h"
+#include <fstream>
+#include <iostream>
 
-#include <stdio.h>
+using namespace std;
 
-int main(int argc , char** argv)
-{
-	unsigned char RGB[3];
-
-	int i = 0;
-	for(i = 0 ; i < 255 ; i++)
-	{
-		HS_getColorMapFromIntensity(i * 256 , RGB);
-		printf("%03d R : %03d G : %03d , B : %03d\n", i, RGB[0] , RGB[1] , RGB[2]);
+int main(){
+	ifstream ifile("../lidardata.pcap");
+	if(!ifile){
+		cerr<<"open failed"<<endl;
+		return 0;
 	}
+
+	ifile.seekg(0,ifile.end);
+	int length = ifile.tellg();
+	ifile.seekg(0,ifile.beg);
+	cout<<"file length: "<<length<<" bytes"<<endl;
+
+	unsigned char buf[16];
+
+	ofstream ofile("o.dat");
+	// for(int i=0;i<10;i++){
+	// 	//cout<<ifile.get()<<endl;
+	// 	//ofile<<ifile.get();
+	// 	buf[i] = ifile.get();
+	// 	ofile<<buf[i];
+	// }
+
+	//ifile.seekg(0,ifile.beg);
+	char *bufp = new char [length];
+	ifile.read(bufp,length);
+	int bufstart = 0;
+	int bufend = 0;
+	//while(bufend<length){
+		//unsigned short tmp = bufp[bufstart] & 0xff | ((bufp[bufstart+1] & 0xff) << 8);
+		unsigned char a = (bufp[0]);
+	// unsigned char a=0xd4;
+	printf("%x\n",a);
+		cout<<hex<<a<<endl;
+		unsigned short tmp = ((bufp[bufstart] & 0xff )<< 8)| ((bufp[bufstart+1] & 0xff)  );
+	//}
+	cout<<hex<<tmp<<endl;
+
+	//cout<<"asdad"<<memcmp(bufp,&tmp,2)<<endl;
+
+	ofile.write(bufp,length);
+	//ofile<<bufp;
+	cout<<ofile.good()<<ifile.good();
+	cout<<"ffee pos: "<<ifile.tellg()<<endl;
+
+	// ifile.seekg(0,ifile.beg);
+	// unsigned short tmp; 
+	// while(tmp!=0xffee){
+	// 	ifile.read((char*)&tmp,2);
+	// 	cout<<hex<<tmp<<endl;
+	// }
+	// cout<<"ffee pos: "<<ifile.tellg()<<endl;
+
+	delete[] bufp;
 	return 0;
 }
