9214e5768a85d0bfdb9191d18078c3fdfd6ad420
wangzp <wzhpanging@163.com>
2018-03-16 18:55:27 +0800

    add ros

---
+++ ROS_Tuto/catkin_ws/build/beginner_tuto/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/wangzp/code/Github/everything/ROS_Tuto/catkin_ws/src/beginner_tuto
+# Build directory: /home/wangzp/code/Github/everything/ROS_Tuto/catkin_ws/build/beginner_tuto
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ ROS_Tuto/catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/wangzp/code/Github/everything/ROS_Tuto/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/wangzp/code/Github/everything/ROS_Tuto/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/wangzp/code/Github/everything/ROS_Tuto/catkin_ws/devel;/opt/ros/kinetic" != "" else []
